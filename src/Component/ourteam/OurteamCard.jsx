7613c68feb3e623d2050d8f247eaf2fa94825b9a
Zaman Ahmad <zamanahmad763@gmail.com>
2023-06-14 08:19:40 +0530

    fifth

--- src/Component/ourteam/OurteamCard.jsx
+++ src/Component/ourteam/OurteamCard.jsx
@@ -5,7 +5,7 @@ const OurteamCard = () => {
         <>
             <div className="col-our-team">
                 <img src="https://media.istockphoto.com/id/1434947698/photo/working-computer-email-and-business-man-from-china-serious-about-fixing-a-laptop-glitch.webp?b=1&s=170667a&w=0&k=20&c=TJKjKgbUxYKPPV6f_M28K2Khtoz_34K4l3FfL5LAd7E=" alt="" 
-                style={{width: '100%', height:'250px'}} />
+                />
                 <div className='zoom'></div>
             </div>
         </>
